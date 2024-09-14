c7d7d3695d7d213a4effee67ecbc1b6305821619
yelantsevv <yelantsevv@gmail.com>
2024-09-14 11:35:26 +0300

    74

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -125,8 +125,10 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const x = rect1.left + rect1.width > rect2.left;
+  const y = rect1.top + rect1.height > rect2.top;
+  return x && y;
 }
 
 /**
@@ -155,8 +157,10 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const x = (point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2;
+  if (x < circle.radius ** 2) return true;
+  return false;
 }
 
 /**
